940aea5dfd0d2e51c7fb44ccbdc93996bb6f3dfb
balajikadambi <balaji.kadambi@gmail.com>
2025-01-23 11:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask,request,jsonify
+from flask import Flask
 import os
-import http.client
-import json
-#from flask_cors import CORS
-
 
 app = Flask(__name__)
-#CORS(app);
 
 @app.route('/')
 def hello():
     return "Hello World!"
-    
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
