1eaf9c8b81d5c968fc9ba6d67b39a37b1473af4b
Ayyappadas G B <44163764+edward795@users.noreply.github.com>
2025-01-23 13:01:40 +0530

    feat : Update app.py -> hide secrets apikey & projectid

--- app.py
+++ app.py
@@ -11,18 +11,6 @@ ibm_cloud__iam_url="iam.cloud.ibm.com"
 ibm_watsonx_url="au-syd.ml.cloud.ibm.com"
 ibm_watsonx_model_generation_api="/ml/v1-beta/generation/text?version=2023-05-29"
 
-apikey = ""
-project_id = ""
-
-conn_ibm_cloud_iam = http.client.HTTPSConnection(ibm_cloud__iam_url)
-payload = "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey="+apikey
-headers = { 'Content-Type': "application/x-www-form-urlencoded" }
-conn_ibm_cloud_iam.request("POST", "/identity/token", payload, headers)
-res = conn_ibm_cloud_iam.getresponse()
-data = res.read()
-decoded_json=json.loads(data.decode("utf-8"))
-access_token=decoded_json["access_token"]
-
 conn_watsonx = http.client.HTTPSConnection(ibm_watsonx_url)
 CORS(app)
 
@@ -32,21 +20,22 @@ context="Technical Troubleshooting"
 def queryWatsonx():
     query_arg=request.args['query']
     print("query : ",query_arg)
-    try:
-        str_payload=create_payload(context, query_arg)
-        out_json=watsonx_generate(str_payload)
+    # try:
+    str_payload,access_token=create_payload(context, request.args)
+    out_json=watsonx_generate(str_payload,access_token)
 
-        answer=out_json["results"]
-        json_output=""
-        for count, ele in enumerate(answer):
-            print (f"Answer {count} = {answer[count]['generated_text']}")
-            json_output+=f"Answer {count} = {answer[count]['generated_text']}"
-        return jsonify(json_output)
-    except:
-        return jsonify("An error occurred while fetching the response from watsonx.ai service!!!")
+    print("output",out_json)
+    answer=out_json["results"]
+    json_output=""
+    for count, ele in enumerate(answer):
+        print (f"Answer {count} = {answer[count]['generated_text']}")
+        json_output+=f"Answer {count} = {answer[count]['generated_text']}"
+    return jsonify(json_output)
+    # except:
+    #     return jsonify("An error occurred while fetching the response from watsonx.ai service!!!")
 
 #function to genereate the paylod to invoke watson.ai service
-def watsonx_generate(payload):
+def watsonx_generate(payload,access_token):
 
     headers = {
         'Authorization': "Bearer "+access_token,
@@ -61,7 +50,22 @@ def watsonx_generate(payload):
     decoded_json=json.loads(data.decode("utf-8"))
     return (decoded_json)
 
-def create_payload(context, question):
+def create_payload(context, args):
+    print("args:",args)
+    print("a[ikey :]",args['apikey'])
+    apikey = args['apikey']
+    project_id = args['projectid']
+    question=args['query']
+
+    conn_ibm_cloud_iam = http.client.HTTPSConnection(ibm_cloud__iam_url)
+    payload = "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey="+apikey
+    headers = { 'Content-Type': "application/x-www-form-urlencoded" }
+    conn_ibm_cloud_iam.request("POST", "/identity/token", payload, headers)
+    res = conn_ibm_cloud_iam.getresponse()
+    data = res.read()
+    decoded_json=json.loads(data.decode("utf-8"))
+    access_token=decoded_json["access_token"]
+    
     payload_json_flan_ul2 = { "model_id": "ibm/granite-3-8b-instruct",
   "input": context + "Input: " + question + "  Output:",
   "parameters": {
@@ -75,7 +79,7 @@ def create_payload(context, question):
     }
 
     str_payload=json.dumps(payload_json_flan_ul2)
-    return str_payload
+    return [str_payload,access_token]
 
 #function to capture & return the generated results
 def getTopAnswer(context, question):
